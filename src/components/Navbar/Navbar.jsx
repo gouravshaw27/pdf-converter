6704f0aeabf197c225f4c9399175c3cfcef58d64
Harsh Yadav <harshyadav@Harshs-MacBook-Air.local>
2023-11-05 08:47:47 +0530

    Made the navbar feature as you intended

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import '../Assets/img 1.jpg'
 
 const Navbar = () => {
 
-  const[home,setHome] = useState('Home');
+  const[activeTab,setActiveTab] = useState('Home');
   
   return (
     <div className="navbar">
@@ -16,10 +16,10 @@ const Navbar = () => {
       </div>
       
         <ul className="nav-menu">
-          <li onClick={()=>{setHome('Home')}}>Home {home==='Home'?<hr/>:<></>}</li>
-          <li onClick={()=>{setHome('Service')}}>Service{home==='Service'?<hr/>:<></>}</li>
-          <li onClick={()=>{setHome('About')}}>About{home==='About'?<hr/>:<></>}</li>
-          <li onClick={()=>{setHome('Contact-us')}}>Contact-us{home==='Contact-us'?<hr/>:<></>}</li>
+          <li className={activeTab==='Home'?'menu-item-active':""} onClick={()=>{setActiveTab('Home')}}>Home {activeTab==='Home'?<div></div>:<></>}</li>
+          <li className={activeTab==='Service'?'menu-item-active':""} onClick={()=>{setActiveTab('Service')}}>Service{activeTab==='Service'?<div></div>:<></>}</li>
+          <li className={activeTab==='About'?'menu-item-active':""} onClick={()=>{setActiveTab('About')}}>About{activeTab==='About'?<div></div>:<></>}</li>
+          <li className={activeTab==='Contact-us'?'menu-item-active':""} onClick={()=>{setActiveTab('Contact-us')}}>Contact-us{activeTab==='Contact-us'?<div></div>:<></>}</li>
         </ul>
       <div className="log-in">
         <button className='btn btn-success'>Log-in</button></div>
