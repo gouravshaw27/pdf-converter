bf0256efcd37223a53fbf2ff71a650ff5bc68386
Harsh Yadav <harshyadav@Harshs-MacBook-Air.local>
2023-11-11 10:21:37 +0530

    choose file button started working

--- src/pages/Convert.jsx
+++ src/pages/Convert.jsx
@@ -7,16 +7,17 @@ const Convert = () => {
       <div className="head">
         <h1>PDF Converter</h1>
       </div>
-      <div className="input-box">
+      <form  className='input-box'>
       <label className="input">
         <input type="file" />
-        <button className='choose-file'>CHOOSE FILES</button>
-        <button title="Expand menu"className="expand">^</button>
+        <button className='choose-file' type='button' >CHOOSE FILES</button>
+        <button title="Expand menu" type='button' className="expand">^</button>
       </label>
-      </div>
+      </form>
         <div className='text'>
           <p>or drop files here</p>
           </div>
+          
       <div className="para">
         <h5>Convert files from and to PDF easily with our free PDF converter—no sign-up needed. Just drag and drop your files to get started.</h5>
       </div>
